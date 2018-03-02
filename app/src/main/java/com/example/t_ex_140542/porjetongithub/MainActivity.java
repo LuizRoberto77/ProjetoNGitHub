35bc77378e49bb487ec1577284b993de5517abc8
Luiz Roberto <roberto-hero@hotmail.com>
2018-03-02 12:01:17 -0300

    Alterações no layout,cores e BottomNavigation

---
+++ app/src/main/java/com/example/t_ex_140542/porjetongithub/MainActivity.java
@@ -0,0 +1,48 @@
+package com.example.t_ex_140542.porjetongithub;
+
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottomNav);
+
+        bottomNavigationView.setOnNavigationItemReselectedListener(new BottomNavigationView.OnNavigationItemReselectedListener() {
+            @Override
+            public void onNavigationItemReselected(@NonNull MenuItem item) {
+
+                switch (item.getItemId()) {
+                    case R.id.action_recents:
+                        Toast toast = Toast.makeText(MainActivity.this, "Clique para recentes", Toast.LENGTH_LONG);
+                        toast.setGravity(Gravity.CENTER_HORIZONTAL | Gravity.CENTER_VERTICAL, 0, 0);
+                        toast.show();
+
+                        break;
+                    case R.id.action_favorite:
+                        Toast toast1 = Toast.makeText(MainActivity.this, "Clique para favoritos", Toast.LENGTH_LONG);
+                        toast1.setGravity(Gravity.CENTER_HORIZONTAL | Gravity.CENTER_VERTICAL, 0, 0);
+                        toast1.show();
+
+                        break;
+
+                    case R.id.action_nearby:
+                        Toast toast2 = Toast.makeText(MainActivity.this, "Clique para locais", Toast.LENGTH_LONG);
+                        toast2.setGravity(Gravity.CENTER_HORIZONTAL | Gravity.CENTER_VERTICAL, 0, 0);
+                        toast2.show();
+
+                        break;
+                }
+            }
+        });
+    }
+}
